c7924ae5ae482f4ca6074ed9b86e5bc9723e5a2f
Vitalii-Pavliuk <vitalikpavlyuk67@gmail.com>
2026-01-19 14:31:25 +0200

    Add/C#

--- src/constants/tunings.ts
+++ src/constants/tunings.ts
@@ -10,7 +10,7 @@ const NOTES: Record<string, number> = {
   Fs2: 92.50, G2: 98.00, Gs2: 103.83, A2: 110.00, As2: 116.54, B2: 123.47,
   C3: 130.81, Cs3: 138.59, D3: 146.83, Ds3: 155.56, E3: 164.81, F3: 174.61,
   Fs3: 185.00, G3: 196.00, Gs3: 207.65, A3: 220.00, As3: 233.08, B3: 246.94,
-  C4: 261.63, D4: 293.66, E4: 329.63
+  C4: 261.63,  Cs4: 277.18, D4: 293.66, E4: 329.63
 };
 
 export const TUNINGS: Tuning[] = [
@@ -31,5 +31,12 @@ export const TUNINGS: Tuning[] = [
     name: 'Drop C',
     strings: [NOTES.C2, NOTES.G2, NOTES.C3, NOTES.F3, NOTES.A3, NOTES.D4],
     stringNames: ['C', 'G', 'C', 'F', 'A', 'D']
-  }
+  }, 
+  {
+    id: 'standard_c_sharp',
+    name: 'C# Standard',
+    strings: [NOTES.Cs2, NOTES.Fs2, NOTES.B2, NOTES.E3, NOTES.Gs3, NOTES.Cs4],
+    stringNames: ['C#', 'F#', 'B', 'E', 'G#', 'C#']
+  },
+  
 ];
